cab9d162d82e734b7d26ede6b601a85aa3fa431a
TasukuNakano <task1010243@gmail.com>
2023-09-20 16:47:07 +0900

    add paid_holidays form

--- app/controllers/paid_holidays_controller.rb
+++ app/controllers/paid_holidays_controller.rb
@@ -2,4 +2,23 @@ class PaidHolidaysController < ApplicationController
   def index
     @paid_holidays = PaidHoliday.all
   end
+
+  def new
+    @paid_holiday = PaidHoliday.new
+  end
+
+  def create
+    @paid_holiday = PaidHoliday.new(paid_holiday_params)
+
+    if @paid_holiday.save
+      redirect_to @paid_holiday
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def paid_holiday_params
+    params.require(:paid_holiday).permit(:used_worker, :first_period, :end_period, :type, :used_days, :substitute_worker1, :substitute_worker2)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   root "workers#index"
 
   resources :workers
+  resources :paid_holidays
 end
