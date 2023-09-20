1e365acbd0efa146e0ee767c715c171df0d7595e
TasukuNakano <task1010243@gmail.com>
2023-09-20 16:46:49 +0900

    fix type name error

--- app/models/paid_holiday.rb
+++ app/models/paid_holiday.rb
@@ -3,5 +3,6 @@ class PaidHoliday < ApplicationRecord
   validates :first_period, presence: true
   validates :end_period, presence: true
   validates :type, presence: true
+  self.inheritance_column = :_type_disabled
   validates :used_days, presence: true
 end
