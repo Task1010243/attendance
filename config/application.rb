1c5d4695ac7be0d1a83419e94bd148ae453dce7d
TasukuNakano <task1010243@gmail.com>
2023-09-27 09:33:46 +0900

    replace simple_form

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,9 @@ module Attendance
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.i18n.available_locales = %i[ja]
     config.i18n.default_locale = :ja
     config.time_zone = 'Asia/Tokyo'
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    #config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
